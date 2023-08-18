c57f3ba377000c0d2fcb79d81314e3b1d0f516c1
Timothy itayi <timmytam10@gmail.com>
2023-08-18 21:49:19 +1200

    add tailwind styling

--- blog/components/ui/auth/LoginButton.tsx
+++ blog/components/ui/auth/LoginButton.tsx
@@ -4,7 +4,14 @@ import { useAuth0 } from '@auth0/auth0-react'
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0()
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>
+  return (
+    <button
+      onClick={() => loginWithRedirect()}
+      className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded shadow"
+    >
+      Log In
+    </button>
+  )
 }
 
 export default LoginButton
--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -3,23 +3,27 @@ import { useAuth0 } from '@auth0/auth0-react'
 import LoginButton from '../components/ui/auth/LoginButton'
 import LogoutButton from '../components/ui/auth/LogoutButton'
 
-const Home: React.FC = () => {
+const LoginPage: React.FC = () => {
   const { isAuthenticated, user } = useAuth0()
 
   return (
-    <div>
-      <h1>Welcome to My Recipe Blog</h1>
-      <p>This is the homepage of the recipe blog application.</p>
-      {isAuthenticated ? (
-        <div>
-          <p>{user?.name}</p>
-          <LogoutButton />
-        </div>
-      ) : (
-        <LoginButton />
-      )}
+    <div className="flex flex-col items-center justify-center h-screen bg-blue-500">
+      <div className="bg- p-8 rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-4">Welcome to My Recipe Blog</h1>
+        <p className="mb-8">
+          This is the homepage of the recipe blog application.
+        </p>
+        {isAuthenticated ? (
+          <div>
+            <p className="mb-2">Hello, {user?.name}!</p>
+            <LogoutButton />
+          </div>
+        ) : (
+          <LoginButton />
+        )}
+      </div>
     </div>
   )
 }
 
-export default Home
+export default LoginPage
