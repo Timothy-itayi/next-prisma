52c0689b0cf5a522ed61a3ff7104beaa98cbc2ec
Gracie Sharkey <graciesharkey@Gracies-MacBook-Pro-2.local>
2023-08-19 11:34:42 +1200

    Committing my changes before applying the codemod

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -8,26 +8,23 @@ const SignInPage: React.FC = () => {
 
   const handleLogin = () => {
     loginWithRedirect().then(() => {
-      // After successful login, navigate to the landing page
-      window.location.href = '/logged-in/landing';
+      // After successful login, use the Link component to navigate to the landing page
     });
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-blue-200 font-mono">
-  
-    
-        <div className="flex flex-col items-center justify-center h-screen bg-blue-200">
-    
-      <h1 className="text-2xl font-bold mb-4">Welcome</h1>
-      <p className="mb-8">Please sign in</p>
-      <LoginButton onClick={handleLogin} />
+      <div className="flex flex-col items-center justify-center h-screen bg-blue-200">
+        <h1 className="text-2xl font-bold mb-4">Welcome</h1>
+        <p className="mb-8">Please sign in</p>
+        <LoginButton onClick={handleLogin} />
+        <Link href="/logged-in/landing" legacyBehavior>
+  {/* The text acts as the link */}
+  <a className="mt-4 text-blue-500 hover:underline">Go to Landing Page</a>
+</Link>
+
+      </div>
     </div>
-  </div>
-     
-    
-  
-  
   );
 };
 
