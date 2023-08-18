b134d281f75a5cc68638a1e675749dde0e93171a
Timothy itayi <timmytam10@gmail.com>
2023-08-18 18:33:26 +1200

    succesfully intergrated auth0 in next.js app

--- blog/pages/_app.tsx
+++ blog/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { Auth0Provider } from '@auth0/auth0-react'
 import { AppProps } from 'next/app'
-import '../app/styles/global.css' // Adjust the path based on your structure
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const redirectUri =
+    typeof window !== 'undefined' ? window.location.origin : ''
+
   return (
     <Auth0Provider
       domain="dev-kuvlvwpp7p78xckw.au.auth0.com"
       clientId="dMfZUZJaJ8b8MfA4eRr8TLVfNU1Copli"
       authorizationParams={{
-        redirect_uri: window.location.origin,
+        redirect_uri: redirectUri,
       }}
     >
       <Component {...pageProps} />
