f234a7038921c665a649da464b4649c81e26da3a
Timothy itayi <timmytam10@gmail.com>
2023-08-18 14:10:31 +1200

    create recipe blog v2

---
+++ blog/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Recipe Blog</h1>
+      <p>This is the homepage of the recipe blog application.</p>
+    </div>
+  )
+}
+
+export default Home
