a3936ef50c9dc6f4d0b9825ae1aa1a6ca29014d6
Gracie Sharkey <graciesharkey@Gracies-MacBook-Pro-2.local>
2023-08-19 08:40:27 +1200

    successfully configure tailwind styling

--- blog/pages/_app.tsx
+++ blog/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Auth0Provider } from '@auth0/auth0-react'
 import { AppProps } from 'next/app'
-import '../src/styles/globals.css'
+import '../src/styles/globals.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
   const redirectUri =
     typeof window !== 'undefined' ? window.location.origin : ''
