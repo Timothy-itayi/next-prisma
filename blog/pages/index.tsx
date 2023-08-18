d3648feef81260123d5f67613a6b707d8b0864b2
Timothy itayi <timmytam10@gmail.com>
2023-08-18 19:01:47 +1200

    login and logout authenticated user successfully

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -4,7 +4,7 @@ import LoginButton from '../components/ui/auth/LoginButton'
 import LogoutButton from '../components/ui/auth/LogoutButton'
 
 const Home: React.FC = () => {
-  const { isAuthenticated } = useAuth0()
+  const { isAuthenticated, user } = useAuth0()
 
   return (
     <div>
@@ -12,7 +12,7 @@ const Home: React.FC = () => {
       <p>This is the homepage of the recipe blog application.</p>
       {isAuthenticated ? (
         <div>
-          <p>You are logged in!</p>
+          <p>{user?.name}</p>
           <LogoutButton />
         </div>
       ) : (
