52b1bcf4fb467f565b3f48f66b4b6f9a9bc33037
Gracie Sharkey <timmytam10@gmail.com>
2023-08-23 15:59:19 +1200

    update settings.json

--- blog/pages/about.tsx
+++ blog/pages/about.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next';
 const AboutPage: NextPage = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>test Page</h1>
       <p>This is the about page of your application.</p>
     </div>
   );
